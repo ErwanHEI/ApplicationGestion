80ac1ee552b55ea4acbfb7a3a5f25eb1ceb9ec04
Benjamin Leignel <Benjamin Leignel>
2016-04-27 10:53:49 +0200

    Signed-off-by: Benjamin Leignel <Benjamin Leignel> update 4

---
+++ src/view/PanelEvenement.java
@@ -0,0 +1,241 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+import modelTableau.ModeleTableauListeBudget;
+import modelTableau.ModeleTableauListeProduit;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+
+public class PanelEvenement extends JPanel{
+
+	private JTextField champReferenceEvenement;
+	private JTextField champNomEvenement;
+	private JTextField champDateEvenement;
+	private JTextField champBudgetPrevu;
+	
+	private JButton boutonCreerEvenement;
+	
+	private ModeleTableauListeProduit modeleListeProduit = new ModeleTableauListeProduit();
+	private ModeleTableauListeBudget modeleListeBudget = new ModeleTableauListeBudget();
+	private JTable tableauProduitEvenement;
+
+	
+	public PanelEvenement() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 16, 1002, 665);
+		this.setLayout(new BorderLayout());
+		this.add(tabbedPane,BorderLayout.CENTER);
+		
+		JPanel ongletEvenement = new JPanel();
+		ongletEvenement.setBackground(new Color(51, 102, 204));
+		tabbedPane.addTab("Ev\u00E8nements", null, ongletEvenement, null);
+		ongletEvenement.setLayout(null);
+		
+		JPanel panelEvenement = new JPanel();
+		panelEvenement.setBounds(0, 0, 997, 631);
+		ongletEvenement.add(panelEvenement);
+		
+/*----------------------------------------------------------------------------------------------
+---------------------------------------------TABLEAU DES BUDGETS--------------------------------
+------------------------------------------------------------------------------------------------*/
+		//tableauEvenement = new JTable(modeleListeBudget);
+
+				
+/*----------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------*/	
+		
+		JPanel ongletGestion = new JPanel();
+		ongletGestion.setBackground(new Color(51, 102, 204));
+		tabbedPane.addTab("Gestion", null, ongletGestion, null);
+		ongletGestion.setLayout(null);
+		
+		JPanel panelCreationEvenement = new JPanel();
+		panelCreationEvenement.setBounds(0, 0, 997, 230);
+		ongletGestion.add(panelCreationEvenement);
+		panelCreationEvenement.setLayout(null);
+		
+		JLabel labelCreationEvenement = new JLabel("Cr\u00E9ation d'un \u00E9v\u00E8nement");
+		labelCreationEvenement.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		labelCreationEvenement.setBounds(15, 16, 334, 37);
+		panelCreationEvenement.add(labelCreationEvenement);
+		
+		JLabel labelReferenceEvenement = new JLabel("R\u00E9f\u00E9rence :");
+		labelReferenceEvenement.setForeground(Color.BLACK);
+		labelReferenceEvenement.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		labelReferenceEvenement.setBounds(323, 69, 102, 24);
+		panelCreationEvenement.add(labelReferenceEvenement);
+		
+		champReferenceEvenement = new JTextField();
+		champReferenceEvenement.setColumns(10);
+		champReferenceEvenement.setBounds(440, 70, 400, 26);
+		panelCreationEvenement.add(champReferenceEvenement);
+		
+		JLabel labelNomEvenement = new JLabel("Nom :");
+		labelNomEvenement.setForeground(Color.BLACK);
+		labelNomEvenement.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		labelNomEvenement.setBounds(371, 109, 54, 24);
+		panelCreationEvenement.add(labelNomEvenement);
+		
+		champNomEvenement = new JTextField();
+		champNomEvenement.setColumns(10);
+		champNomEvenement.setBounds(440, 110, 400, 26);
+		panelCreationEvenement.add(champNomEvenement);
+		
+		JLabel labelDateEvenement = new JLabel("Date de commencement :");
+		labelDateEvenement.setForeground(Color.BLACK);
+		labelDateEvenement.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		labelDateEvenement.setBounds(193, 149, 232, 24);
+		panelCreationEvenement.add(labelDateEvenement);
+		
+		champDateEvenement = new JTextField();
+		champDateEvenement.setColumns(10);
+		champDateEvenement.setBounds(440, 150, 400, 26);
+		panelCreationEvenement.add(champDateEvenement);
+		
+		JLabel labelBudgetPrevu = new JLabel("Budget pr\u00E9vu :");
+		labelBudgetPrevu.setForeground(Color.BLACK);
+		labelBudgetPrevu.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		labelBudgetPrevu.setBounds(293, 189, 132, 24);
+		panelCreationEvenement.add(labelBudgetPrevu);
+		
+		champBudgetPrevu = new JTextField();
+		champBudgetPrevu.setColumns(10);
+		champBudgetPrevu.setBounds(440, 190, 400, 26);
+		panelCreationEvenement.add(champBudgetPrevu);
+		
+		JLabel labelOptionnel = new JLabel("(optionnel)");
+		labelOptionnel.setBounds(855, 193, 78, 20);
+		panelCreationEvenement.add(labelOptionnel);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 246, 997, 369);
+		ongletGestion.add(panel);
+		panel.setLayout(null);
+		
+		
+		
+/*----------------------------------------------------------------------------------------------
+---------------------------------------------TABLEAU DES PRODUITS--------------------------------
+------------------------------------------------------------------------------------------------*/			
+		JLabel labelProduitNecessaire = new JLabel("Produits n\u00E9cessaires :");
+		labelProduitNecessaire.setBounds(15, 16, 194, 24);
+		panel.add(labelProduitNecessaire);
+		labelProduitNecessaire.setForeground(Color.BLACK);
+		labelProduitNecessaire.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+		tableauProduitEvenement = new JTable(modeleListeProduit);
+		tableauProduitEvenement.getColumnModel().getColumn(6).setMaxWidth(0);
+		tableauProduitEvenement.getColumnModel().getColumn(6).setMinWidth(0);
+		tableauProduitEvenement.getColumnModel().getColumn(6).setPreferredWidth(0);
+		
+		JScrollPane scrollPane = new JScrollPane(tableauProduitEvenement);
+		scrollPane.setBounds(0, 48, 997, 259);
+		panel.add(scrollPane, BorderLayout.CENTER);
+		
+		boutonCreerEvenement = new JButton("Cr\u00E9er");
+		boutonCreerEvenement.setBounds(387, 323, 220, 30);
+		panel.add(boutonCreerEvenement);
+		boutonCreerEvenement.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		
+/*----------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------*/
+		
+		JPanel ongletJournal = new JPanel();
+		tabbedPane.addTab("Journal", null, ongletJournal, null);
+	}
+
+	public JTextField getChampReferenceEvenement() {
+		return champReferenceEvenement;
+	}
+
+	public void setChampReferenceEvenement(JTextField champReferenceEvenement) {
+		this.champReferenceEvenement = champReferenceEvenement;
+	}
+
+	public JTextField getChampNomEvenement() {
+		return champNomEvenement;
+	}
+
+	public void setChampNomEvenement(JTextField champNomEvenement) {
+		this.champNomEvenement = champNomEvenement;
+	}
+
+	public JTextField getChampDateEvenement() {
+		return champDateEvenement;
+	}
+
+	public void setChampDateEvenement(JTextField champDateEvenement) {
+		this.champDateEvenement = champDateEvenement;
+	}
+
+	public JTextField getChampBudgetPrevu() {
+		return champBudgetPrevu;
+	}
+
+	public void setChampBudgetPrevu(JTextField champBudgetPrevu) {
+		this.champBudgetPrevu = champBudgetPrevu;
+	}
+
+	public JButton getBoutonCreerEvenement() {
+		return boutonCreerEvenement;
+	}
+
+	public void setBoutonCreerEvenement(JButton boutonCreerEvenement) {
+		this.boutonCreerEvenement = boutonCreerEvenement;
+	}
+
+	public ModeleTableauListeProduit getModeleListeProduit() {
+		return modeleListeProduit;
+	}
+
+	public void setModeleListeProduit(ModeleTableauListeProduit modeleListeProduit) {
+		this.modeleListeProduit = modeleListeProduit;
+	}
+
+	public ModeleTableauListeBudget getModeleListeBudget() {
+		return modeleListeBudget;
+	}
+
+	public void setModeleListeBudget(ModeleTableauListeBudget modeleListeBudget) {
+		this.modeleListeBudget = modeleListeBudget;
+	}
+
+	public JTable getTableauProduitEvenement() {
+		return tableauProduitEvenement;
+	}
+
+	public void setTableauProduitEvenement(JTable tableauProduitEvenement) {
+		this.tableauProduitEvenement = tableauProduitEvenement;
+	}
+	
+	
+}
+
